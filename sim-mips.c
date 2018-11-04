b68055609dd7380b12cf5706aae5fe4522f286ce
Marisa Liu <myliu@umass.edu>
2018-11-04 20:55:18 +0000

    starting parser

--- sim-mips.c
+++ sim-mips.c
@@ -13,7 +13,7 @@ everything is in offsets
 **offsets(in terms of words) are in relation to the next line of where they are called
 
 read through fgets
-instructions limited to 100 instructions
+instructions limited to 100 characters
 
 2 modes
   single cycle mode
@@ -33,27 +33,70 @@ fp = fopen(filename, "r");
 char *currentLine;
 char *line;
 
+///Structure that holds data about the type of instruction 
+struct inst{
+  char* type;
+  int opcode;
+  int rs;
+  int rt;
+  int rd;
+  int Imm;
+}
+
+
 
 char progScanner(currentLine){
 line = strtok(currentLine, " ,()");           //split by spaces, commas, parentheses
 }
-  
-  take in file
- parse line by line
- should be able to take in names and numbers - eg. zero and 0
- remove duplicate commas, spaces, parenthesises, etc.
- keep reading until halt simulation
+ 
+char *regNumberConverter(){
+   /*goes through line until reaches $ 
+    * take the string to the right of the $ until whitespace
+    *if(t s a v k)
+        if second letter is number
+	  if no third letter convert register to appropriate number
+	    if(first letter is t)
+	       if(second letter is 0 - 7) reg = 8 + second letter
+	       else reg = 16 + second letter
+	    elseif(first letter is v) 
+	       if(second letter is 0 or 1) reg = 2 + second letter
+	       else report error
+	    elseif(first letter is a)
+	      if(second letter is 0-3) reg = 4 + second letter
+	      else report error
+	    elseif(first letter is s)
+	      if(second letter is 0 - 7) reg = 16 + second letter
+	      else report error
+	    else
+	      if(second letter is 0 or 1) reg = 26 + second letter
+	      else report error
+	  else report error
+	else if(string is at) convert to 1
+	else if(string is sp) convert to 29
+	else report error
+     else if(gp) convert to 28
+     else if(fp) convert to 30
+     else if(ra) convert to 31
+     else if(zero) convert to 0
+     else if(0 - 31) delete $ from before 
+     else return error
+    * */
 
-char *regNumberConverter()
-  takes in output of progScanner
-  returns pointer to character string, which all register names are converted to numbers
+}
+//////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////PARSER/////////////////////////////////////////////////////
+//////Parameter --> output of regNumberConvereter()///////////////////////////////////
+//////Return --> inst struct with fields for each of the files of MIPS instructions///
+//////////////////////////////////////////////////////////////////////////////////////
+struct inst parser(char *line){
+  inst *newInst;
+  /*parse by whitespace and create an array of strings/numbers
+   if(index 0 is ADD, SUB, MULT) --> contains opcode, rs, rt, rd, funct
 
-struct inst parser()
-  uses output of regNumberConverter()
-  returns instruction as inst struct with fields for each of the fields of MIPS instructions
-    eg. opcode, rs, rt, rd, Imm.
-	 not all fields will be used for each line
-  if illegal opcode, error in simulation and stops
+}
+
+
+
 
 void IF(){
 
